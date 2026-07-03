2aeb90a371d98455c63f2f5d4bf8e05deb44f6de
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-07-03 15:48:01 +0900

    ユーザ情報変更ページ(途中)

--- php/profile.php
+++ php/profile.php
@@ -1,5 +1,7 @@
 <?php
-
+    require_once "db.php";
+    require_once "auth.php";
+    start_sess();
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +10,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='../css/footer.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'>
+    <script src='https://cdn.tailwindcss.com'></script>
 </head>
 <body>
-    
+    <?php include "header.php"; ?>
+    <main>
+        <h1>ユーザ情報変更</h1>
+        <p>ユーザ名：<?php echo $_SESSION["username"] ?></p>
+        <p>パスワード：******</p>
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                echo "";
+            }
+        ?>
+        <form action="post">
+            <button type="submit" name="change">変更する<button>
+        </form>
+    </main>
+    <?php include "footer.php"?>
 </body>
 </html>
